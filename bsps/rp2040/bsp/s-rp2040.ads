b4393ac137b16663d8d917bef3aa696b7fa01395
Hedley Rainnie <hedley@qbert.dynalias.com>
2021-02-28 17:14:05 -0800

    - initial rp2040
    Signed-off-by: Hedley Rainnie <hedley@qbert.dynalias.com>

---
+++ bsps/rp2040/bsp/s-rp2040.ads
@@ -0,0 +1,34 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT RUN-TIME COMPONENTS                         --
+--                                                                          --
+--          Copyright (C) 2012-2016, Free Software Foundation, Inc.         --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License along  --
+-- with this library; see the file COPYING3. If not, see:                   --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  This file provides register definitions for the STM32F103 (ARM Cortex M3)
+--  microcontrollers from ST Microelectronics.
+
+with Interfaces.Bit_Types;
+
+package System.RP2040 is
+   pragma No_Elaboration_Code_All;
+   pragma Preelaborate (System.RP2040);
+
+   subtype Frequency is Interfaces.Bit_Types.Word;
+
+end System.RP2040;
